79b06f81d5441818a961bdcedfe0fd091d011018
taanngent <72206563+taanngent@users.noreply.github.com>
2021-10-05 18:36:54 +0530

    Create LEETCODE.JAVA

---
+++ Java/LEETCODE.JAVA
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return bsearch(nums,target,0,nums.length-1);
+    }
+    public int bsearch(int []nums, int target, int low, int high)
+    {
+        if(high<low)
+            return -1;
+        int mid = low+(high-low)/2;
+        if(nums[mid]==target)
+            return mid;
+        if(nums[mid]>target)
+            return bsearch(nums,target,low,mid-1);
+        return bsearch(nums,target,mid+1,high);
+    }
+}
